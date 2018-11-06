15976292a6d3f174864fa49c526156d6a8e3f6e2
Jenkins <cajenkins@smu.edu>
2018-11-06 15:26:46 -0600

    random

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -5,42 +5,30 @@
 // top 10 most bought games on xbox this year
 
 // some variables
-var game;
-var price;
-var genre;
-var poular;
-var agerest;
+// var game;
+// var price;
+// var genre;
+// var popular;
+// var agerest;
 
-function buildQueryURL() {
-    // queryURL is the url we'll use to query the API
-    var queryURL = "https://www.giantbomb.com";
+// function buildQueryURL() {
+//     // queryURL is the url we'll use to query the API
+//     var queryURL = "https://www.giantbomb.com";
   
     // Begin building an object to contain our API call's query parameters
     // Set the API key
-    var queryParams = { "api-key": "020f014925d48c42945182a55ffb88c3935919d5 " };
+    // var queryParams = { "api-key": "020f014925d48c42945182a55ffb88c3935919d5" };
 
     // Logging the URL so we have access to it for troubleshooting
 //   console.log("---------------\nURL: " + queryURL + "\n---------------");
 //   console.log(queryURL + $.param(queryParams));
 //   return queryURL + $.param(queryParams);
 
-}
+
 
 // I want this to create a list of 10 games
 // Yummy aren't they  ;D   PROJECTONE\theterminators\assets\images\mmmQtips.gif
 // inside class joke for my TA
-
-$.ajax({
-    url: "https://cors-anywhere.herokuapp.com/https://api-endpoint.igdb.com/genres/",
-    headers: {
-        "user-key": "b22a38958f254adf3bafb79cc8be854e",
-        Accept: "application/json"
-    }
-}).then(function(resp) {
-    console.log(resp);
-}).catch(function(err) {
-    console.log(err);
-})
 
 
 // well it is displaying, just not on the page
@@ -52,13 +40,13 @@ $.ajax({
 
 // here is what q says I do
 // dropdown element
-function getSelectedValue()
-{
-    var selectedValue = document.getElementById("list").value;
-    console.log(selectedValue)
-    return selectedValue;
-}
-// on click listener to make api calls
+// function getSelectedValue()
+// {
+//     var selectedValue = document.getElementById("list").value;
+//     console.log(selectedValue)
+//     return selectedValue;
+// }
+// // on click listener to make api calls
 
 // https://stackoverflow.com/questions/43671008/html-javascript-calling-a-function-with-drop-down-options
 // hardcode all genres on form
@@ -75,12 +63,10 @@ var buttons = ["Popular", "Price", "Age Restriction", "Genre"];
 
 $("button").on("click", function() {
 
-
-    var queryURL = "https://www.giantbomb.com";
-    var genreID = getSelectedValue();
-//     var games = $(this).attr("data-games");
-//     var queryURL = "https://api-endpoint.igdb.com" + games;
-
+var games = $(this).attr("data-games");
+    var queryURL = "https://www.giantbomb.com/api/genres/?api_key=020f014925d48c42945182a55ffb88c3935919d5";
+    // var genreID = getSelectedValue();
+    
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -88,8 +74,6 @@ $("button").on("click", function() {
 
         var results = response.data;
     
-})
+        
 });
-
-
-// dc4003169689b712bf832dde037f1a228cd30496
+})
